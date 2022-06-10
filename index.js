a988ef37fc868740ef076e15878cb50771ca83d8
moses ngige <moses32ngigz@gmail.com>
2022-06-10 11:08:20 +0300

    commit tree 6775a304572300f1ecebc22605400bbf802f93cd

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+function findMatching(names, string) {
+    return names.filter(
+      (possibility) => possibility.toLowerCase() === string.toLowerCase()
+    );
+  }
+  
+function fuzzyMatch(names, string) {
+    return names.filter(
+      (possibility) =>
+        possibility.toLowerCase().indexOf(string.toLowerCase()) === 0
+    );
+  }
+  
+function matchName(array, string) {
+    return array.filter((record) => record.name === string);
+  }
